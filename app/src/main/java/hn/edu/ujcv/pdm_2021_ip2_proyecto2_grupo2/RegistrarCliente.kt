04580e2291c0945a09953d090541aeca6da9ca78
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-03-20 12:44:54 -0600

    Registrar Cliente

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ip2_proyecto2_grupo2/RegistrarCliente.kt
@@ -0,0 +1,15 @@
+package hn.edu.ujcv.pdm_2021_ip2_proyecto2_grupo2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistrarCliente : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_cliente)
+
+
+    }
+}
