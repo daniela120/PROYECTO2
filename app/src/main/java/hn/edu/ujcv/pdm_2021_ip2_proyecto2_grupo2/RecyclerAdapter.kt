edfc1f4c55bdab4249b9faa423dba3dc48d8b1ff
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-20 15:40:55 -0600

    recycler

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ip2_proyecto2_grupo2/RecyclerAdapter.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ip2_proyecto2_grupo2/RecyclerAdapter.kt
@@ -14,21 +14,23 @@ import androidx.recyclerview.widget.RecyclerView
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     private val titles = arrayOf("Registrar Cliente", "Registrar Menu", "Registrar Mesa",
-            "Registrar Empleado", "Realizar Pedido",
-            "Realizar Factura", "Enviar por correo")
+        "Registrar Empleado", "Realizar Pedido",
+        "Realizar Factura", "Enviar por correo")
 
     private val details = arrayOf( "Registrar un Cliente", "Registrar un Menu", "Registrar una Mesa",
-            "Registrar un Empleado", "Realizar un Pedido",
-            "Realizar una  Factura", "Enviar Factura por correo")
+        "Registrar un Empleado", "Realizar un Pedido",
+        "Realizar una  Factura", "Enviar Factura por correo")
 
     private val images = intArrayOf(
-            R.drawable.registro_cliente,
-            R.drawable.registrar_menu, R.drawable.mesa,
-            R.drawable.empleado, R.drawable.pedido,
-            R.drawable.factura, R.drawable.enviar)
+        R.drawable.registro_cliente,
+        R.drawable.registrar_menu, R.drawable.mesa,
+        R.drawable.empleado, R.drawable.pedido,
+        R.drawable.factura, R.drawable.enviar)
+
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val v =LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        val v = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_layout, viewGroup, false)
         return ViewHolder(v)
     }
 
@@ -36,19 +38,26 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         var itemImage: ImageView
         var itemTitle: TextView
         var itemDetail: TextView
-
+        
         init {
             itemImage=itemView.findViewById(R.id.item_image)
             itemTitle=itemView.findViewById(R.id.item_title)
             itemDetail=itemView.findViewById(R.id.item_detail)
 
-            itemView.setOnClickListener{ v: View->
-                var position: Int = getAdapterPosition()
+
+            itemView.setOnClickListener { v: View? ->
+                var position:Int = getAdapterPosition()
+                println(position.toString())
 
 
                 when(position){
                     0 -> GoRegistrarCliente(itemView)
                     1 -> GoRegistrarMenu(itemView)
+                    2 -> GoRegistrarMesa(itemView)
+                    3 -> GoRegistrarEmpleado(itemView)
+                    4 -> GoRealizarPedido(itemView)
+                    5 -> GoRealizarFactura(itemView)
+                    6 -> GoEviarFactura(itemView)
                     else->GoMain(itemView)
 
                 }
@@ -64,14 +73,36 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         itemView.context.startActivity(intent)
 
     }
-
-    fun GoRegistrarMenu(itemView: View) {
-        val intent = Intent(itemView.context, MainActivity::class.java)
+    fun GoRegistrarCliente(itemView: View) {
+        val intent = Intent(itemView.context, RegistrarCliente::class.java)
         itemView.context.startActivity(intent)
     }
 
-    fun GoRegistrarCliente(itemView: View) {
-        val intent = Intent(itemView.context, RegistrarCliente ::class.java)
+    fun GoRegistrarMenu(itemView: View) {
+        val intent = Intent(itemView.context, RegistrarMenu::class.java)
+        itemView.context.startActivity(intent)
+    }
+    fun GoRegistrarMesa(itemView: View) {
+        val intent = Intent(itemView.context, RegistrarMesa::class.java)
+        itemView.context.startActivity(intent)
+    }
+
+    fun GoRegistrarEmpleado(itemView: View) {
+        val intent = Intent(itemView.context, RegistrarEmpleado::class.java)
+        itemView.context.startActivity(intent)
+    }
+    fun GoRealizarFactura(itemView: View) {
+        val intent = Intent(itemView.context,MainActivity::class.java)
+        itemView.context.startActivity(intent)
+    }
+
+    fun GoRealizarPedido(itemView: View) {
+        val intent = Intent(itemView.context, MainActivity ::class.java)
+        itemView.context.startActivity(intent)
+    }
+
+    fun GoEviarFactura(itemView: View) {
+        val intent = Intent(itemView.context, MainActivity ::class.java)
         itemView.context.startActivity(intent)
     }
 
@@ -86,4 +117,3 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         return titles.size
     }
 }
-
