98c4257886d83ca4f4113d62e805339f2fd7a1fa
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-03-20 17:02:32 -0600

    .

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ip2_proyecto2_grupo2/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ip2_proyecto2_grupo2/MainActivity.kt
@@ -1,14 +1,17 @@
 package hn.edu.ujcv.pdm_2021_ip2_proyecto2_grupo2
 
+import android.graphics.Color
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.size
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.activity_main.*
-import android.graphics.Color
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
@@ -19,13 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        collapsing_toolbar.title = "LA COCINA DE MEXICO"
-        collapsing_toolbar.setContentScrimColor(Color.BLACK)
+        collapsing_toolbar.title = "La Cocina \n de Mexico"
+        collapsing_toolbar.setContentScrimColor(Color.BLUE)
 
         layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter=adapter
-        }
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
